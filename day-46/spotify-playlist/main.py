92f15c5090d7e0686591c2cb46d5cf60c5bb9980
Hyun Jung KANG <clinguist.hjkang@gmail.com>
2021-02-15 11:23:02 +0900

    add

--- day-46/spotify-playlist/main.py
+++ day-46/spotify-playlist/main.py
@@ -1,16 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 URL = "https://www.billboard.com/charts/hot-100/"
+APP_CLIENT_ID = "04c50db127864289aca312a997df4f9f"
+APP_CLIENT_SECRET = "474ab4bb89a244cca016378a55c99e3f"
+APP_REDIRECT_URI = "http://example.com"
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-# date = "2000-08-08"
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+date = "2000-08-08"
 
 response = requests.get(URL+date)
 web_page = response.text
 
 soup = BeautifulSoup(web_page, "html.parser")
 
-titles = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
-titles_list = [title.getText() for title in titles]
-print(titles_list)
+songs = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
+song_names = [song.getText() for song in songs]
+
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=APP_CLIENT_ID,
+                                               client_secret=APP_CLIENT_SECRET,
+                                               redirect_uri=APP_REDIRECT_URI,
+                                               scope="playlist-modify-private",
+                                               show_dialog=True,
+                                               cache_path="token.txt",
+                                               ))
+
+user_id = sp.current_user()["id"]
+
+
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
